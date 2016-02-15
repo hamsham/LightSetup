a0fd1bf615403e648995327caebeb500d5666e5d
hamsham <mileslacey@gmail.com>
2016-02-15 02:37:22 -0800

    Added the ability to retrieve a target platform's default byte-size and pointer size.

--- include/lightsky/setup/Setup.h
+++ include/lightsky/setup/Setup.h
@@ -9,6 +9,8 @@
 #ifndef __LS_SETUP_SETUP_H__
 #define __LS_SETUP_SETUP_H__
 
+#include <climits>
+
 #include "lightsky/setup/Compiler.h"
 #include "lightsky/setup/Arch.h"
 #include "lightsky/setup/OS.h"
@@ -23,7 +25,65 @@ enum class platform_t {
     MOBILE
 };
 
-} // end platform namespace
+
+
+/**
+ * @brief Get the number of bits per byte on the current platform.
+ * 
+ * @return The number of bits (typically 8) that are natively stored in a
+ * single byte on the current platform.
+ */
+constexpr unsigned get_bit_length() {
+    return CHAR_BIT;
+}
+
+
+
+/**
+ * @brief Retrieve the current platform's byte length.
+ * 
+ * @return An unsigned integer containing the number of bits per byte. This
+ * number is typically 8.
+ */
+constexpr unsigned get_byte_length() {
+    return sizeof(char);
+}
+
+
+
+/**
+ * @brief Retrieve the current platform's pointer size.
+ * 
+ * @return An unsigned integer containing the number of bits that are required
+ * to store a pointer. This is typically 32 or 64 bits.
+ */
+constexpr unsigned get_num_platform_bits() {
+    return sizeof(void*) * get_bit_length();
+}
+
+
+
+/**
+ * @brief Determine if the current platform is 32-bit.
+ * 
+ * @return TRUE if the current platform is 32-bit, FALSE if otherwise.
+ */
+constexpr bool is_32_bit() {
+    return 32 == get_num_platform_bits();
+}
+
+
+
+/**
+ * @brief Determine if the current platform is 64-bit.
+ * 
+ * @return TRUE if the current platform is 64-bit, FALSE if otherwise.
+ */
+constexpr bool is_64_bit() {
+    return 64 == get_num_platform_bits();
+}
+
+
 
 /**
  * @brief Helper function to determine if the current platform is Mobile or
@@ -32,9 +92,12 @@ enum class platform_t {
  * @return ls::platform::platform_t::DESKTOP if the current build is targeting
  * a desktop platform, MOBILE if otherwise.
  */
-ls::platform::platform_t get_target_platform();
+platform_t get_target_platform();
 
+} // end platform namespace
 } // end ls namespace
+
+
 
 /*
  * LS_PLATFORM
@@ -46,7 +109,8 @@ ls::platform::platform_t get_target_platform();
 
 #elif defined (LS_OS_ANDROID) || defined (LS_OS_IOS)
     #define LS_PLATFORM ls::platform::platform_t::MOBILE
-
 #endif /* LS_PLATFORM */
+
+
 
 #endif /* __LS_SETUP_SETUP_H__ */
